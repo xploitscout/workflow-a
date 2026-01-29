4000856ad7797603186887e6619656252f48d357
xploitscout <xploitscout@wearehackerone.com>
2026-01-29 23:15:07 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import { workflow } from "@vercel/workflow";
 
-export const testWorkflow = workflow("test-a", async () => {
-  return "SECRET_FROM_A";
+export const secretWorkflow = workflow("secret-workflow-a", async () => {
+  return {
+    secret: "VERY_SECRET_FROM_PROJECT_A"
+  };
 });
+
