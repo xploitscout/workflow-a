186ebce86010cd193a7de7e7a8a15f7d32278a2a
xploitscout <xploitscout@wearehackerone.com>
2026-01-29 22:30:42 +0500

    Update run.ts

--- api/run.ts
+++ api/run.ts
@@ -6,7 +6,5 @@ const myWorkflow = workflow("test-a", async () => {
 
 export default async function handler(req, res) {
   const run = await myWorkflow.run();
-  res.json({
-    runId: run.id
-  });
+  res.status(200).json({ runId: run.id });
 }
